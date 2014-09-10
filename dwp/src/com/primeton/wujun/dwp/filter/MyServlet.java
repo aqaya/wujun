33e9fc2b4c9bfd0d1fa05269f2aa0eb8a307f666
wujun <wujun@primeton.com>
2014-09-10 16:39:58 +0800

    init

---
+++ dwp/src/com/primeton/wujun/dwp/filter/MyServlet.java
@@ -0,0 +1,41 @@
+package com.primeton.wujun.dwp.filter;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//response.setContentType("application/json;charset=utf-8");
+		response.getWriter().append("Hello ");
+		System.out.println("middle");
+		response.getWriter().print("world!");
+	}
+
+}
---
+++ dwp/src/com/primeton/wujun/dwp/servlet/MyFilter.java
@@ -0,0 +1,49 @@
+package com.primeton.wujun.dwp.servlet;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class MyFilter
+ */
+@WebFilter("/*")
+public class MyFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public MyFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("Before MyFilter!");
+		chain.doFilter(request, response);
+		System.out.println("After MyFilter!");
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
