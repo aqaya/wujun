9b1446914034b08160468a29bee145042671afc5
wujun <wujun@primeton.com>
2014-09-26 14:56:35 +0800

    update

--- MyPythonTest/com/primeton/wujun/python/test/test.py
+++ MyPythonTest/com/primeton/wujun/python/test/test.py
@@ -4,16 +4,6 @@ Created on 2014年9月26日
 
 @author: wjaxm_000
 '''
-import os
+import datetime
+print datetime.datetime.now()-datetime.datetime.now()
 
-def anyTrue(predicate, sequence):
-    return True in map(predicate, sequence)
-
-def filterFiles(folder, exts):
-    for fileName in os.listdir(folder):
-        fileName = fileName.decode("gbk").encode("utf-8")
-        if os.path.isdir(folder + '/' + fileName):
-            filterFiles(folder + '/' + fileName, exts)
-        elif anyTrue(fileName.endswith, exts):
-            print fileName
-filterFiles("f:/ebook", ["pdf"])
