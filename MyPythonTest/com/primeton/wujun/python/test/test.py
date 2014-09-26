7668181e92003dc85a0b47345b871b647d3bc5ea
wujun <wujun@primeton.com>
2014-09-26 14:48:39 +0800

    update

--- MyPythonTest/com/primeton/wujun/python/test/test.py
+++ MyPythonTest/com/primeton/wujun/python/test/test.py
@@ -11,7 +11,9 @@ def anyTrue(predicate, sequence):
 
 def filterFiles(folder, exts):
     for fileName in os.listdir(folder):
+        fileName = fileName.decode("gbk").encode("utf-8")
         if os.path.isdir(folder + '/' + fileName):
             filterFiles(folder + '/' + fileName, exts)
         elif anyTrue(fileName.endswith, exts):
             print fileName
+filterFiles("f:/ebook", ["pdf"])
