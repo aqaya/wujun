1e9b4fa91147d82e0573707e6cd8831f9dba1817
wujun <wujun@primeton.com>
2014-12-15 16:18:53 +0800

    update

--- com.primeton.wujun.restful.webapp/src/main/java/com/primeton/wujun/com/primeton/wujun/restful/webapp/MyResource.java
+++ com.primeton.wujun.restful.webapp/src/main/java/com/primeton/wujun/com/primeton/wujun/restful/webapp/MyResource.java
@@ -1,9 +1,18 @@
 package com.primeton.wujun.com.primeton.wujun.restful.webapp;
 
+import java.net.URI;
+
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.server.spi.Container;
 
 /**
  * Root resource (exposed at "myresource" path)
@@ -18,8 +27,36 @@ public class MyResource {
      * @return String that will be returned as a text/plain response.
      */
     @GET
+    @Path("/a/{username}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Got it!";
+    public String getIt(@DefaultValue("2") @QueryParam("step") int step,
+    		@PathParam("username") String userName, 
+    		@DefaultValue("true") @QueryParam("min-m") boolean hasMin) {
+        return "Got a!" + userName+step+hasMin;
     }
+    
+    @GET
+    @Path("/b")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt2() {
+        return "Got b!";
+    }
+    
+    @PUT
+    public Response putContainer() {
+		return null;
+//        System.out.println("PUT CONTAINER " + container);
+//        URI uri = uriInfo.getAbsolutePath();
+//        Container c = new Container(container, uri.toString());
+//        Response r;
+//        if (!MemoryStore.MS.hasContainer(c)) {
+//            r = Response.created(uri).build();
+//        } else {
+//            r = Response.noContent().build();
+//        }
+//        MemoryStore.MS.createContainer(c);
+//        return r;
+    }
+
+   
 }
