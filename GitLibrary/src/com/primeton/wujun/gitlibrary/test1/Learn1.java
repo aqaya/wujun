d6af9659b549b4c2d04697ce22e5f0b110fa0c8d
wujun <wujun@primeton.com>
2015-01-23 15:12:18 +0800

    update:修改编码

--- GitLibrary/src/com/primeton/wujun/gitlibrary/test1/Learn1.java
+++ GitLibrary/src/com/primeton/wujun/gitlibrary/test1/Learn1.java
@@ -2,6 +2,6 @@ package com.primeton.wujun.gitlibrary.test1;
 
 public class Learn1{
 	public static void main(String[] args){
-		System.out.println("我要去水果店买水果！");
+		System.out.println("鎴戣鍘绘按鏋滃簵涔版按鏋滐紒");
 	}
 }
