87a58e2f6e6708c92db60f9d82b5ab7ff4fa4418
wujun <wujun@primeton.com>
2015-01-23 15:38:21 +0800

    Signed-off-by: wujun <wujun@primeton.com>

--- GitLibrary/src/com/primeton/wujun/gitlibrary/test1/Learn1.java
+++ GitLibrary/src/com/primeton/wujun/gitlibrary/test1/Learn1.java
@@ -3,7 +3,7 @@ package com.primeton.wujun.gitlibrary.test1;
 public class Learn1{
 	public static void main(String[] args){
 		System.out.println("我要去水果店买水果！");
-		System.out.println("我要去水果店买香蕉2！");
+		System.out.println("我要去水果店买香蕉3！");
 		int d; 
 	}
 }
